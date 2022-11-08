205f4018ad2661da1b64d443e2f02c7e4da0866b
PresidentBarry <108346495+PresidentBarry@users.noreply.github.com>
2022-11-08 15:44:45 +0000

    Add files via upload

--- Coursework/Form1.cs
+++ Coursework/Form1.cs
@@ -14,6 +14,7 @@ namespace Coursework
 
     public partial class Form1 : Form
     {
+        Bitmap DrawArea;
         //Wood variables
         double wood = 0;
         double money = 50;
@@ -25,6 +26,9 @@ namespace Coursework
         public Form1()
         {
             InitializeComponent();
+            DrawArea = new Bitmap(pictureBox1.Size.Width, pictureBox1.Size.Height);
+            pictureBox1.Image = DrawArea;
+            pictureBox1.BackgroundImage = Coursework.Properties.Resources.grass;
             money1.Text = "Money: $" + money;
         }
 
@@ -47,7 +51,7 @@ namespace Coursework
                     woodMult = woodMult * 2;
                 }
                 woodLvlUp = (int)(woodLvlUp * 1.95);
-                tile1.Text = "Level " + woodLvl + " plantation. Produces " + woodMult + " wood per sec. Upgrade cost: $" + woodLvlUp;
+                a1.Text = "Level " + woodLvl + " plantation. Produces " + woodMult + " wood per sec. Upgrade cost: $" + woodLvlUp;
             }
         }
 
@@ -60,7 +64,7 @@ namespace Coursework
 
         private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
         {
-            btn_WoodCount.Text = "Wood: " + wood;
+            btn_logCount.Text = "Wood: " + wood;
             money1.Text = "Money: $" + money;
         }
 
@@ -97,43 +101,38 @@ namespace Coursework
         private void wood1_Click(object sender, EventArgs e)
         {
             money = money + wood;
-            btn_WoodCount.Text = "Wood: " + wood;
+            btn_logCount.Text = "Wood: " + wood;
             money1.Text = "Money: $" + money;
             wood = 0;
         }
 
         private void btn_newTile_Click(object sender, EventArgs e)
         {
-            //switch (tileCount)
-            //{
-            //    case 2:
-            //        tile2.Visible = true;
-            //        break;
-            //    case 3:
-            //        tile3.Visible = true;
-            //        break;
-            //    case 4:
-            //        tile4.Visible = true;
-            //        break;
-            //    case 5:
-            //        tile5.Visible = true;
-            //        break;
-            //    case 6:
-            //        tile6.Visible = true;
-            //        break;
-            //}
 
-            //for (int i = 0; i < 84; i++)
-            //{
-            //    string tile = String.Concat("tile", i);
-            //    $tile
-            //}
-            
+            Graphics g;
+            g = Graphics.FromImage(DrawArea);
 
-            //for (int i = 0; i < 84; i++)
-            //{
-            //    tile(i).visible = true;
-            //}
+            Pen mypen = new Pen(Color.Black);
+
+            int a = 10;
+            int b = 10;
+            int c = 100;
+            int d = 100;
+
+            for (int j = 0; j < 9; j++)
+            {
+
+                for (int i = 0; i < 16; i++)
+                {
+                    g.DrawRectangle(mypen, a, b, c, d);
+
+                    a = a + 110;
+                }
+                a = 10;
+                b = b + 110;
+            }
+
+            pictureBox1.Image = DrawArea;
 
             lbl_choice.Visible = true;
             btn_choiceCancel.Visible = true;
@@ -144,29 +143,29 @@ namespace Coursework
 
         private void btn_tileChoicePlantation_Click(object sender, EventArgs e)
         {
-            switch (tileCount)
-            {
-                case 2:
-                    tile2.Text = "Plantation";
-                    tile2.BackColor = Color.SaddleBrown;
-                    break;
-                case 3:
-                    tile3.Text = "Plantation";
-                    tile3.BackColor = Color.SaddleBrown;
-                    break;
-                case 4:
-                    tile4.Text = "Plantation";
-                    tile4.BackColor = Color.SaddleBrown;
-                    break;
-                case 5:
-                    tile5.Text = "Plantation";
-                    tile5.BackColor = Color.SaddleBrown;
-                    break;
-                case 6:
-                    tile6.Text = "Plantation";
-                    tile6.BackColor = Color.SaddleBrown;
-                    break;
-            }
+            //switch (tileCount)
+            //{
+            //    case 2:
+            //        a2.Text = "Plantation";
+            //        a2.BackColor = Color.SaddleBrown;
+            //        break;
+            //    case 3:
+            //        a3.Text = "Plantation";
+            //        a3.BackColor = Color.SaddleBrown;
+            //        break;
+            //    case 4:
+            //        a4.Text = "Plantation";
+            //        a4.BackColor = Color.SaddleBrown;
+            //        break;
+            //    case 5:
+            //        a5.Text = "Plantation";
+            //        a5.BackColor = Color.SaddleBrown;
+            //        break;
+            //    case 6:
+            //        a6.Text = "Plantation";
+            //        a6.BackColor = Color.SaddleBrown;
+            //        break;
+            //}
             lbl_choice.Visible = false;
             btn_choiceCancel.Visible = false;
             btn_choicePlantation.Visible = false;
@@ -177,29 +176,29 @@ namespace Coursework
 
         private void btn_tileChoiceMine_Click(object sender, EventArgs e)
         {
-            switch (tileCount)
-            {
-                case 2:
-                    tile2.Text = "Mine";
-                    tile2.BackColor = Color.BlueViolet;
-                    break;
-                case 3:
-                    tile3.Text = "Mine";
-                    tile3.BackColor = Color.BlueViolet;
-                    break;
-                case 4:
-                    tile4.Text = "Mine";
-                    tile4.BackColor = Color.BlueViolet;
-                    break;
-                case 5:
-                    tile5.Text = "Mine";
-                    tile5.BackColor = Color.BlueViolet;
-                    break;
-                case 6:
-                    tile6.Text = "Mine";
-                    tile6.BackColor = Color.BlueViolet;
-                    break;
-            }
+            //switch (tileCount)
+            //{
+            //    case 2:
+            //        a2.Text = "Mine";
+            //        a2.BackColor = Color.BlueViolet;
+            //        break;
+            //    case 3:
+            //        a3.Text = "Mine";
+            //        a3.BackColor = Color.BlueViolet;
+            //        break;
+            //    case 4:
+            //        a4.Text = "Mine";
+            //        a4.BackColor = Color.BlueViolet;
+            //        break;
+            //    case 5:
+            //        a5.Text = "Mine";
+            //        a5.BackColor = Color.BlueViolet;
+            //        break;
+            //    case 6:
+            //        a6.Text = "Mine";
+            //        a6.BackColor = Color.BlueViolet;
+            //        break;
+            //}
             lbl_choice.Visible = false;
             btn_choiceCancel.Visible = false;
             btn_choicePlantation.Visible = false;
@@ -210,29 +209,29 @@ namespace Coursework
 
         private void btn_tileChoiceFarm_Click(object sender, EventArgs e)
         {
-            switch (tileCount)
-            {
-                case 2:
-                    tile2.Text = "Farm";
-                    tile2.BackColor = Color.OrangeRed;
-                    break;
-                case 3:
-                    tile3.Text = "Farm";
-                    tile3.BackColor = Color.OrangeRed;
-                    break;
-                case 4:
-                    tile4.Text = "Farm";
-                    tile4.BackColor = Color.OrangeRed;
-                    break;
-                case 5:
-                    tile5.Text = "Farm";
-                    tile5.BackColor = Color.OrangeRed;
-                    break;
-                case 6:
-                    tile6.Text = "Farm";
-                    tile6.BackColor = Color.OrangeRed;
-                    break;
-            }
+            //switch (tileCount)
+            //{
+            //    case 2:
+            //        a2.Text = "Farm";
+            //        a2.BackColor = Color.OrangeRed;
+            //        break;
+            //    case 3:
+            //        a3.Text = "Farm";
+            //        a3.BackColor = Color.OrangeRed;
+            //        break;
+            //    case 4:
+            //        a4.Text = "Farm";
+            //        a4.BackColor = Color.OrangeRed;
+            //        break;
+            //    case 5:
+            //        a5.Text = "Farm";
+            //        a5.BackColor = Color.OrangeRed;
+            //        break;
+            //    case 6:
+            //        a6.Text = "Farm";
+            //        a6.BackColor = Color.OrangeRed;
+            //        break;
+            //}
             lbl_choice.Visible = false;
             btn_choiceCancel.Visible = false;
             btn_choicePlantation.Visible = false;
@@ -243,24 +242,24 @@ namespace Coursework
 
         private void btn_choiceCancel_Click(object sender, EventArgs e)
         {
-            switch (tileCount)
-            {
-                case 2:
-                    tile2.Visible = false;
-                    break;
-                case 3:
-                    tile3.Visible = false;
-                    break;
-                case 4:
-                    tile4.Visible = false;
-                    break;
-                case 5:
-                    tile5.Visible = false;
-                    break;
-                case 6:
-                    tile6.Visible = false;
-                    break;
-            }
+            //switch (tileCount)
+            //{
+            //    case 2:
+            //        a2.Visible = false;
+            //        break;
+            //    case 3:
+            //        a3.Visible = false;
+            //        break;
+            //    case 4:
+            //        a4.Visible = false;
+            //        break;
+            //    case 5:
+            //        a5.Visible = false;
+            //        break;
+            //    case 6:
+            //        a6.Visible = false;
+            //        break;
+            //}
             lbl_choice.Visible = false;
             btn_choiceCancel.Visible = false;
             btn_choicePlantation.Visible = false;
@@ -298,6 +297,16 @@ namespace Coursework
         private void lbl_upgradesTile_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void exit_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(string.Format("X: {0} Y: {1}", MousePosition.X, MousePosition.Y));
         }
     }
 }
